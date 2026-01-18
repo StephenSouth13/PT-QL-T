31ca3481bc8b148fca0d9752d396c42b1eb61c4f
Quách Thành Long <stephensouth1307@gmail.com>
2026-01-18 16:35:19 +0700

    da
    upd

---
+++ Buoi_2/Buoi2/Baitapbuoi2/Tài liệu hướng dẫn.R
@@ -0,0 +1,20 @@
+library(readxl)
+library(dplyr)
+
+# Thử với .xlsx
+df <- read_excel("D:/UEH/PT-QL-T/Buoi_2/stock_prices.xlsx", sheet = "Prices")
+# 2) Đảm bảo Date là Date và sắp theo thời gian
+df <- df %>%
+  mutate(Date = as.Date(Date)) %>%
+  arrange(Date)
+
+# 3) Tính return từ Close
+df <- df %>%
+  mutate(
+    ret_close = Close / lag(Close) - 1,          # simple return
+    logret_close = log(Close / lag(Close))       # log return
+  )
+
+# 4) Xem kết quả
+head(df, 10)
+summary(df$ret_close)
